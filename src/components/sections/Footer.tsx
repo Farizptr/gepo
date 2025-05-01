c0b3f20fe096ac2533664e16d5d29160c2c453b0
Farizptr <18221015@std.stei.itb.ac.id>
2025-05-01 15:24:44 +0700

    update:pages

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -75,18 +75,18 @@ const Footer: React.FC = () => {
             <p className="text-base mb-4">
               Masukkan email Anda untuk mendapatkan pembaruan terbaru dari Gepo Energy
             </p>
-            <form onSubmit={handleSubmit} className="flex items-center">
+            <form onSubmit={handleSubmit} className="flex items-center w-3/4">
               <input
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
-                className="bg-[#BDBDBD]/30 text-[#BDBDBD] py-2 px-4 rounded-l w-full focus:outline-none text-base placeholder:text-[#BDBDBD]"
+                className="bg-[#BDBDBD]/30 text-[#BDBDBD] h-10 px-4 rounded-l w-full focus:outline-none text-base placeholder:text-[#BDBDBD]"
                 required
               />
               <button
                 type="submit"
-                className="bg-[#BDBDBD]/30 text-[#FFD700] p-2 rounded-r hover:bg-[#BDBDBD]/40 transition duration-300"
+                className="bg-[#BDBDBD]/30 text-[#FFD700] h-10 px-4 rounded-r hover:bg-[#BDBDBD]/40 transition duration-300 flex items-center justify-center"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
